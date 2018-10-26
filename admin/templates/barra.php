a6a756f78ccb846ef138932b50fc253e6dad0194
pabloingino <pablo.ingino@gmail.com>
2018-10-26 09:00:45 -0300

    se sube el de lista duplicado funcionando para pruebas en el otro server

--- admin/templates/barra.php
+++ admin/templates/barra.php
@@ -8,7 +8,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>T</b>CM</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>TECH</b>Camp</span>
+      <span class="logo-lg"><b>TECH</b>./Camp</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
