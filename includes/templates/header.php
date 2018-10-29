29abb7dfc3615fd7de23d123f66064ad066acee2
pingino <pingino@mininterior.gob.ar>
2018-10-29 10:54:32 -0300

    se arreglo el mapa

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -13,6 +13,7 @@
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" />
 
         <?php
             $archivo = basename($_SERVER['PHP_SELF']);
